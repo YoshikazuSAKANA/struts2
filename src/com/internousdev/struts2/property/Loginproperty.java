d18a97dd2e1eb22e5f13487425b0b8afee6ca5c3
internousdev <internousdev@00174045119435>
2017-02-24 15:19:40 +0900

    owari

---
+++ src/com/internousdev/struts2/property/Loginproperty.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.internousdev.struts2.property;
+
+/**
+ * @author internousdev
+ *
+ */
+public class Loginproperty {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/com/internousdev/struts2/util/Loginutil.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.internousdev.struts2.util;
+
+/**
+ * @author internousdev
+ *
+ */
+public class Loginutil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
